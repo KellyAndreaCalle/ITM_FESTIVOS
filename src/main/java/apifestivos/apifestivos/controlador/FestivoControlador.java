84cdb9b616d6443710feff4fab77c6c0fd863343
KellyAndreaCalle <kelly@MacBook-Air-de-Kelly.local>
2024-05-18 22:31:09 -0500

    Se realiza un cambio en el controlador FestivoControlador.java

--- src/main/java/apifestivos/apifestivos/controlador/FestivoControlador.java
+++ src/main/java/apifestivos/apifestivos/controlador/FestivoControlador.java
@@ -1,13 +1,9 @@
-package apifestivos.apifestivos.controlador;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import apifestivos.apifestivos.core.interfaces.implementaciones.IFestivoServicio;
-
 import java.time.DateTimeException;
 import java.time.LocalDate;
 
@@ -22,7 +18,8 @@ public class FestivoControlador {
         try {
             LocalDate fecha = LocalDate.of(año, mes, dia);
             boolean esFestivo = festivoServicio.esFestivo(fecha);
-            return ResponseEntity.ok(esFestivo);
+            String mensaje = esFestivo ? "True" : "False";
+            return ResponseEntity.ok().body(mensaje);
         } catch (DateTimeException e) {
             return ResponseEntity.badRequest().body("Fecha no válida");
         }
